ace8ba6d9406815b37b0879e83e136af4d5258b6
Rebecca Pavelt <rebeccapavelt@gmx.de>
2024-06-28 17:21:48 +0200

    add functionality to form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import './App.css';
+import "./App.css";
+import Form from "./components/Form";
+import { useState } from "react";
 
 function App() {
+  const [activities, setActivities] = useState([]);
+
+  function handleAddActivities(newActivity) {
+    setActivities((prevActivities) => [...prevActivities, { ...newActivity }]);
+  }
+
   return (
-   
+    <div>
+      <Form onAddActivity={handleAddActivities} />
+      <ul>
+        {activities.map((activity) => (
+          <li key={activity.id}>{activity.name}</li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
